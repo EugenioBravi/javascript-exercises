b77952e0805e789782fc5b0995b531822127a620
Eugenio <bravieugenio@gmail.com>
2022-03-15 11:14:14 -0300

    finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+let reversedString = ""; 
 
+const reverseString = function(str) {
+    reversedString = ""; 
+    for(let i = str.length; i > 0; i--){
+        reversedString += str.slice(i - 1, i)
+    }return reversedString
 };
 
 // Do not edit below this line
