ba94fa2a9701751a79b5f176bc5fdbf35660856c
Eugenio <bravieugenio@gmail.com>
2022-03-15 09:44:10 -0300

    finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+let stringRepeated = "";
 
+const repeatString = function(str, times) {
+stringRepeated = "";
+    if(times<0){
+        return 'ERROR'
+    }else{
+        for (let i = 0; i<times; i++){
+        stringRepeated += str;
+    } return stringRepeated;}
 };
 
 // Do not edit below this line
