121aed8426ee0c6c477d5695f15069f35a917cf2
Eugenio <bravieugenio@gmail.com>
2022-03-17 16:33:13 -0300

    finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(fiboNumber) {
+    fiboNumber = parseInt(fiboNumber)
+    let fiboAdd = 0;
+    let fiboResult = 1;
+    if(fiboNumber >= 1){
+        for(let i = 1;i<=fiboNumber;i++){
+            fiboAdd += fiboResult ;
+            fiboResult = fiboAdd-fiboResult;
+        }return fiboAdd;
+    }else if(fiboNumber<0){
+        return "OOPS";
+    }else{
+        return 1;
+    }
+    };
+//1, 1, 2, 3, 5, 8, etc.
 // Do not edit below this line
 module.exports = fibonacci;
