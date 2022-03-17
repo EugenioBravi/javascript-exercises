5c403b870ca7c520b89186ae4c1e8448e7e04961
Eugenio <bravieugenio@gmail.com>
2022-03-17 11:27:27 -0300

    finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+let sumTotal= 0;
+let endNum = 0 ;
+const sumAll = function(startNum, endNum) {
+    sumTotal = 0;
+if(startNum < 0 || typeof endNum != 'number'){
+    return "ERROR"
+} else if(startNum < endNum){  
+    for(let i = startNum; i <= endNum; i++){
+        sumTotal += i;
+    }return sumTotal
+} else{
+    for(let i = startNum; i >= endNum; i--){
+        sumTotal += i;
+    }return sumTotal
+}
 };
 
 // Do not edit below this line
