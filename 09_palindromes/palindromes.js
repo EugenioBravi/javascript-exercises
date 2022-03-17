f86fefd7b2766cb4985357c046fc4fa173f0329d
Eugenio <bravieugenio@gmail.com>
2022-03-17 14:59:13 -0300

    finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+let reversedString = "";
+let pstring = "";
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(     /[\s~`!@#$%^&*()-_+=[\]{}\\|:;"',<>.?\/\u2000-\u206F\u2E00-\u2E7F\u3000-\u303F]/g,     '');
+    pstring = str;
+    reversedString = ""; 
+    for(let i = str.length; i > 0; i--){
+        reversedString += str.slice(i - 1, i)
+    }return pstring == reversedString;
 };
 
 // Do not edit below this line
