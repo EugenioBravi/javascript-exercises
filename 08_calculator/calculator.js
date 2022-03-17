95f0ce22b38d2544e3265757cca9a791a79e3d9c
Eugenio <bravieugenio@gmail.com>
2022-03-17 14:31:43 -0300

    finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+	let total = a + b;
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	let total = a - b;
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function(suma) {
+  let total = 0;
+	for(let i = 0;i < suma.length;i++){
+    total += parseInt(suma[i]);
+  }return total;
 };
 
-const multiply = function() {
+const multiply = function(mul) {
+let total = 1;
+for(let i = 0;i<mul.length;i++){
+total *= mul[i];
+}return total;
 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let total = 1;
+  for(let i = 1;i<= b;i++){
+    total *= parseInt(a);
+  }return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+	for(let i = a; i>1; i--){
+    total *= i;
+  }return total
 };
 
 // Do not edit below this line
