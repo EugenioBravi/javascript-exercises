8f8f37d3422f2c337325d36f96396613f27dfca1
Eugenio <bravieugenio@gmail.com>
2022-03-17 13:22:28 -0300

    finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+let celcius = 0;
+let fahrenheit = 0;
+const ftoc = function(far) {
+celcius = (far - 32) * (5/9);
+return Number(celcius.toPrecision(3));
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+fahrenheit = cel *(9/5) + 32;
+return Number(fahrenheit.toPrecision(4));
 };
 
 // Do not edit below this line
